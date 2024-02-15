ae853aa51807190bd42a03b092678172422cd025
Spencer S <spencer.scarlet@stu.ocsb.ca>
2024-02-15 15:34:04 +0000

    Test Test created

---
+++ com/example/HelloWorldStyle.java
@@ -0,0 +1,28 @@
+package com.example;
+
+/**
+ * Test test test
+ *
+ * @author Test test test
+ * @version 1.0
+ * @since 14-2-2024
+ */
+
+// HelloWorldStyle class
+public final class HelloWorldStyle {
+
+  /** Private constructor to prevent instantiation. */
+  private HelloWorldStyle() {
+    throw new UnsupportedOperationException("Test Test Test1111");
+  }
+
+  /**
+   * This is the main method.
+   *
+   * @param args Unused
+   */
+  public static void main(final String[] args) {
+    System.out.println("Hello, Chris!");
+    System.out.println("Test test test");
+  }
+}
