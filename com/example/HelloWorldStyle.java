f45ca08f4f6245e80092aa28b0f83b1674c3d39d
Spencer S <spencer.scarlet@stu.ocsb.ca>
2024-02-15 16:08:47 +0000

    Teset van

--- com/example/HelloWorldStyle.java
+++ com/example/HelloWorldStyle.java
@@ -1,30 +1,25 @@
 package com.example;
 
 /**
-* Hello world file but with extra style.
-*
-* @author  Spencer S.
-* @version 1.0
-* @since   2024-Feb-08
-*/
-
-
+ * Hello world file but with extra style.
+ *
+ * @author Spencer S.
+ * @version 1.0
+ * @since 2024-Feb-08
+ */
 public final class HelloWorldStyle {
-    /**
-    * Stops random instantiation's from happening.
-    */
-    private HelloWorldStyle() {
-        // Prevents instantiation.
-    }
+  /** Stops random instantiation's from happening. */
+  private HelloWorldStyle() {
+    // Prevents instantiation.
+  }
 
-
-    /**
-    * Main function code.
-    *
-    * @param args Command line arguments (not used).
-    */
-    public static void main(final String[] args) {
-        // Creates the hello world message.
-        System.out.println("Hello world with style in the code.");
-    }
+  /**
+   * Main function code.
+   *
+   * @param args Command line arguments (not used).
+   */
+  public static void main(final String[] args) {
+    // Creates the hello world message.
+    System.out.println("Hello world with style in the code.");
+  }
 }
